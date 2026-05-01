b9b521a2b9d9dc99863a8e8534882031f7568bbd
Yavish Sahrawat <yavisahrawat@gmail.com>
2026-05-01 23:18:19 +0530

    fixed visible differences

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          three: ['three', '@react-three/fiber', '@react-three/drei'],
+        },
+      },
+    },
+  },
 })
